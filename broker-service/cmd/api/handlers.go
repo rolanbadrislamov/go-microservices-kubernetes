c49f680ffcbd983e76c85d487873a6053e089acf
Rolan <badrislamovrolan@gmail.com>
2024-04-14 08:33:08 +0200

    * Implemented a front-end part of the microservice application * Partially developed broker service for the project * Introduced a makefile to automatize commands

---
+++ broker-service/cmd/api/handlers.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
+	payload := jsonResponse{
+		Error:   false,
+		Message: "Hit the broker",
+	}
+
+	out, _ := json.MarshalIndent(payload, "", "\t")
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusAccepted)
+	w.Write(out)
+}
---
+++ broker-service/go.mod
@@ -0,0 +1,8 @@
+module github.com/rolanbadrislamov/go-microservice-kubernetes/broker-service
+
+go 1.21.0
+
+require (
+	github.com/go-chi/chi/v5 v5.0.12
+	github.com/go-chi/cors v1.2.1
+)
